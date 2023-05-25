544cfadd70e0bcf27ff312dcfd2e3a32747d021e
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-05-24 18:52:27 -0600

    add js files for login, logout, and profile

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postsRoutes = require('./postsRoutes');
-const commentsRoutes = require('./commentsRoutes');
+// const commentsRoutes = require('./commentsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postsRoutes);
-router.use('/comments', commentsRoutes);
+// router.use('/comments', commentsRoutes);
 
 module.exports = router;
---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#posts-name').value.trim();
+    const needed_funding = document.querySelector('#posts-funding').value.trim();
+    const description = document.querySelector('#posts-desc').value.trim();
+  
+    if (name && needed_funding && description) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ name, needed_funding, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create posts');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete posts');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-posts-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.posts-list')
+    .addEventListener('click', delButtonHandler);
